01d0eac480ade9b5af1bf02e225cdae24763712c
Artfaal <sys.dll@gmail.com>
2015-10-20 00:06:56 +0300

    refactoring all code

--- app.py
+++ app.py
@@ -1,68 +1,37 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import absolute_import, print_function
-# Trick for unicode symbols
 import sys
+import vk_api
+import tweepy
+import config
+
+# Trick for normal unicode symbols
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-import vk_api
-import config
 
-
-def main():
-    """ Пример получения последнего сообщения со стены """
-
-    login, password = config.LOGIN_VK, config.PASS_VK
-    vk = vk_api.VkApi(login, password)
-
+def authVk():
+    vk = vk_api.VkApi(config.LOGIN_VK, config.PASS_VK)
     try:
         vk.authorization()
     except vk_api.AuthorizationError as error_msg:
-        print(error_msg)
+        print error_msg
         return
-
-    response = vk.method('wall.get', {'owner_id':'-56333679', 'count': 1, 'offset': 2})
-    if response['items']:
-        print(response['items'][0])
+    return vk
 
 
-def twitter():
-
-
-    import tweepy
-
-    # == OAuth Authentication ==
-    #
-    # This mode of authentication is the new preferred way
-    # of authenticating with Twitter.
-
-    # The consumer keys can be found on your application's Details
-    # page located at https://dev.twitter.com/apps (under "OAuth settings")
-    consumer_key = config.TWIKEY
-    consumer_secret = config.TWISECRET
-
-    # The access tokens can be found on your applications's Details
-    # page located at https://dev.twitter.com/apps (located
-    # under "Your access token")
-    access_token = config.TWITOKEN
-    access_token_secret = config.TWITOKENSECRET
-
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+def authTwitter():
+    auth = tweepy.OAuthHandler(config.TWIKEY, config.TWISECRET)
     auth.secure = True
-    auth.set_access_token(access_token, access_token_secret)
-
+    auth.set_access_token(config.TWITOKEN, config.TWITOKENSECRET)
     api = tweepy.API(auth)
+    return api
 
-    # If the authentication was successful, you should
-    # see the name of the account print out
-    print(api.me().name)
 
-    # If the application settings are set for "Read and Write" then
-    # this line should tweet out the message to your account's
-    # timeline. The "Read and Write" setting is on https://dev.twitter.com/apps
-    # api.update_status(status='Updating using OAuth authentication via Tweepy!')
+class getPostFromVk():
+    pass
+
 
 if __name__ == '__main__':
-    # main()
-    twitter()
+    authVk()
+    authTwitter()
